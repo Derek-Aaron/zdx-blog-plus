7bd44f2a6f10de4e2a858b7879405f5235081402
zdx <2488288090@qq.com>
2023-10-03 11:44:47 +0800

    zdx: 表情更改

--- zdx-ui/src/components/Emoji/index.vue
+++ zdx-ui/src/components/Emoji/index.vue
@@ -25,13 +25,13 @@
         <template #footer>
             <div class="emoji-tabs">
                 <div class="emoji-tab" :class="{ 'on': emojiType === 0 }" @click="chooseType(0)">
-                    <img src="https://static.ttkwsd.top/emoji/re.png" width="22" height="22" />
+                    <img src="https://static.zhaodengxuan.top/emoji/嘿嘿.png" width="22" height="22" />
                 </div>
                 <div class="emoji-tab" :class="{ 'on': emojiType === 1 }" @click="chooseType(1)">
-                    <img src="https://static.ttkwsd.top/emoji/tvby.png" width="22" height="22" />
+                    <img src="https://static.zhaodengxuan.top/emoji/tiaopi.gif" width="22" height="22" />
                 </div>
                 <div class="emoji-tab" :class="{ 'on': emojiType === 2 }" @click="chooseType(2)">
-                    <img src="https://static.ttkwsd.top/emoji/yan.png" width="22" height="22">
+                    <img src="@/assets/images/文本.png" width="22" height="22">
                 </div>
             </div>
         </template>
